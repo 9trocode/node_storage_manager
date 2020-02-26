e0d93ed745a76bd05768f1fea5db4f47056d9db0
nitrocode <alexidowu25@gmail.com>
2020-02-26 15:44:27 +0100

    Added AWS sdk functionality and instance parameters

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const GCLOUD = require('./lib/GoogleCloudStorageSystem');
 const NFS = require('./lib/NFSStorageSystem');
+const AWS3 = require('./lib/S3StorageSystem');
 
 class StorageFactory {
-    static getInstance(storageMode) {
+    static getInstance(storageMode, Region) {
         if (!storageMode) {
             throw new Error('Please add a storage Mode');
         }
         if (storageMode === 'GCLOUD') {
             return new GCLOUD();
+        }
+        if (storageMode === 'AWS') {
+            return new AWS3(Region);
         }
         if (storageMode === 'NFS') {
             return new NFS();
